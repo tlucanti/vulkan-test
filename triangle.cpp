f40e8eb302866682800501f6d977b0454c922370
tlucanti <tlucanti@yandex-team.ru>
2024-11-26 00:51:53 +0300

    v2: add 3d positions, depth buffer
    Signed-off-by: tlucanti <tlucanti@yandex-team.ru>

--- triangle.cpp
+++ triangle.cpp
@@ -4,6 +4,7 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -58,6 +59,26 @@ static inline void CALL_VK(VkResult result, std::string message)
 	}
 }
 
+namespace Color {
+	struct ColorImpl {
+		const char *color;
+	};
+
+	ColorImpl blue = { "" };
+	ColorImpl yellow = { "" };
+	ColorImpl red = { "" };
+	ColorImpl reset = { "" };
+
+	#define istty(x) false
+	std::ostream &operator <<(std::ostream &out, const ColorImpl &c)
+	{
+		if (istty(out)) {
+			out << c.color;
+		}
+		return out;
+	}
+};
+
 static VkResult CreateDebugUtilsMessengerEXT(
 	VkInstance instance,
 	const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
@@ -105,11 +126,11 @@ static std::vector<unsigned char> readFile(const std::string &fileName)
 }
 
 struct Vertex {
-	alignas(16) glm::vec2 pos;
+	alignas(16) glm::vec3 pos;
 	alignas(16) glm::vec3 color;
 	alignas(16) glm::vec2 texCoord;
 
-	Vertex(glm::vec2 pos, glm::vec3 color, glm::vec2 texCoord)
+	Vertex(glm::vec3 pos, glm::vec3 color, glm::vec2 texCoord)
 		: pos(pos), color(color), texCoord(texCoord)
 	{}
 
@@ -129,7 +150,7 @@ struct Vertex {
 		VkVertexInputAttributeDescription posDescription = {
 			.location = 0,
 			.binding = 0,
-			.format = VK_FORMAT_R32G32_SFLOAT,
+			.format = VK_FORMAT_R32G32B32_SFLOAT,
 			.offset = offsetof(Vertex, pos),
 		};
 
@@ -164,14 +185,20 @@ struct UniformBufferObject {
 };
 
 const std::vector<Vertex> vertices = {
-	Vertex(glm::vec2(-0.5f, -0.5f), glm::vec3(1, 0, 0), glm::vec2(1, 0)),
-	Vertex(glm::vec2( 0.5f, -0.5f), glm::vec3(0, 1, 0), glm::vec2(0, 0)),
-	Vertex(glm::vec2( 0.5f,  0.5f), glm::vec3(1, 1, 1), glm::vec2(0, 1)),
-	Vertex(glm::vec2(-0.5f,  0.5f), glm::vec3(0, 0, 1), glm::vec2(1, 1)),
+	Vertex(glm::vec3(-0.5f, -0.5f, 0), glm::vec3(1, 0, 0), glm::vec2(1, 0)),
+	Vertex(glm::vec3( 0.5f, -0.5f, 0), glm::vec3(0, 1, 0), glm::vec2(0, 0)),
+	Vertex(glm::vec3( 0.5f,  0.5f, 0), glm::vec3(1, 1, 1), glm::vec2(0, 1)),
+	Vertex(glm::vec3(-0.5f,  0.5f, 0), glm::vec3(0, 0, 1), glm::vec2(1, 1)),
+
+	Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(1, 0, 0), glm::vec2(1, 0)),
+	Vertex(glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(0, 1, 0), glm::vec2(0, 0)),
+	Vertex(glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(1, 1, 1), glm::vec2(0, 1)),
+	Vertex(glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0, 0, 1), glm::vec2(1, 1)),
 };
 
 const std::vector<uint16_t> indices = {
-	0, 1, 2, 2, 3, 0
+	0, 1, 2, 2, 3, 0,
+	4, 5, 6, 6, 7, 4,
 };
 
 class HelloTriangleApplication {
@@ -202,6 +229,9 @@ private:
 	VkImage textureImage;
 	VkDeviceMemory textureImageMemory;
 	VkImageView textureImageView;
+	VkImage depthImage;
+	VkDeviceMemory depthImageMemory;
+	VkImageView depthImageView;
 	VkSampler textureSampler;
 	VkRenderPass renderPass;
 	VkDescriptorSetLayout descriptorSetLayout;
@@ -275,8 +305,9 @@ private:
 		createRenderPass();
 		createDescriptorSetLayout();
 		createGraphicsPipeline();
-		createFramebuffers();
 		createCommandPool();
+		createDepthResources();
+		createFramebuffers();
 		createTextureImage();
 		createTextureImageView();
 		createTextureSampler();
@@ -713,7 +744,7 @@ private:
 		// TODO: check compatibility with render pass and recreate it if needed
 	}
 
-	VkImageView createImageView(VkImage image, VkFormat format)
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags)
 	{
 		VkImageViewCreateInfo imageViewCreateInfo = {
 			.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
@@ -729,7 +760,7 @@ private:
 				.a = VK_COMPONENT_SWIZZLE_IDENTITY,
 			},
 			.subresourceRange = {
-				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.aspectMask = aspectFlags,
 				.baseMipLevel = 0,
 				.levelCount = 1,
 				.baseArrayLayer = 0,
@@ -749,13 +780,14 @@ private:
 		swapChainImageViews.resize(swapChainImages.size());
 
 		for (int i = 0; i < (int)swapChainImages.size(); i++) {
-			swapChainImageViews[i] = createImageView(swapChainImages[i], swapChainImageFormat);
+			swapChainImageViews[i] =
+				createImageView(swapChainImages[i], swapChainImageFormat, VK_IMAGE_ASPECT_COLOR_BIT);
 		}
 	}
 
 	void createTextureImageView(void)
 	{
-		textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8_SRGB);
+		textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
 	}
 
 	void createTextureSampler(void)
@@ -803,9 +835,31 @@ private:
 			.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
 		};
 
+		VkAttachmentDescription depthAttachment = {
+			.flags = 0,
+			.format = findDepthFormat(),
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+			.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+			.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+			.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+		};
+
+		std::array<VkAttachmentDescription, 2> attachments = {
+			colorAttachment,
+			depthAttachment,
+		};
+
 		VkAttachmentReference colorAttachmentRef = {
 			.attachment = 0,
 			.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		};
+
+		VkAttachmentReference depthAttachmentRef = {
+			.attachment = 1,
+			.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
 		};
 
 		VkSubpassDescription subpass = {
@@ -816,7 +870,7 @@ private:
 			.colorAttachmentCount = 1,
 			.pColorAttachments = &colorAttachmentRef,
 			.pResolveAttachments = nullptr,
-			.pDepthStencilAttachment = nullptr,
+			.pDepthStencilAttachment = &depthAttachmentRef,
 			.preserveAttachmentCount = 0,
 			.pPreserveAttachments = nullptr,
 		};
@@ -824,10 +878,13 @@ private:
 		VkSubpassDependency dependency = {
 			.srcSubpass = VK_SUBPASS_EXTERNAL,
 			.dstSubpass = 0,
-			.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-			.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+			.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |
+					VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+			.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |
+					VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
 			.srcAccessMask = 0,
-			.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+			.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT |
+					 VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
 			.dependencyFlags = 0,
 		};
 
@@ -835,8 +892,8 @@ private:
 			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
 			.pNext = nullptr,
 			.flags = 0,
-			.attachmentCount = 1,
-			.pAttachments = &colorAttachment,
+			.attachmentCount = attachments.size(),
+			.pAttachments = attachments.data(),
 			.subpassCount = 1,
 			.pSubpasses = &subpass,
 			.dependencyCount = 1,
@@ -1008,6 +1065,21 @@ private:
 			.alphaToOneEnable = VK_FALSE,
 		};
 
+		VkPipelineDepthStencilStateCreateInfo depthStencil = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.depthTestEnable = VK_TRUE,
+			.depthWriteEnable = VK_TRUE,
+			.depthCompareOp = VK_COMPARE_OP_LESS,
+			.depthBoundsTestEnable = VK_FALSE,
+			.stencilTestEnable = VK_FALSE,
+			.front = {},
+			.back = {},
+			.minDepthBounds = 0.0f,
+			.maxDepthBounds = 1.0f,
+		};
+
 		VkPipelineColorBlendAttachmentState colorBlendAttachment = {
 			.blendEnable = VK_FALSE,
 			.srcColorBlendFactor = VK_BLEND_FACTOR_ONE,
@@ -1058,7 +1130,7 @@ private:
 			.pViewportState = &viewportStateCreateInfo,
 			.pRasterizationState = &rasterizer,
 			.pMultisampleState = &multisampling,
-			.pDepthStencilState = nullptr,
+			.pDepthStencilState = &depthStencil,
 			.pColorBlendState = &colorBlending,
 			.pDynamicState = &dynamicStateCreateInfo,
 			.layout = pipelineLayout,
@@ -1082,8 +1154,9 @@ private:
 		swapChainFramebuffers.resize(swapChainImageViews.size());
 
 		for (int i = 0; i < (int)swapChainImageViews.size(); i++) {
-			VkImageView attachments[] = {
+			std::array<VkImageView, 2> attachments = {
 				swapChainImageViews.at(i),
+				depthImageView,
 			};
 
 			VkFramebufferCreateInfo framebufferCreateInfo = {
@@ -1091,8 +1164,8 @@ private:
 				.pNext = nullptr,
 				.flags = 0,
 				.renderPass = renderPass,
-				.attachmentCount = 1,
-				.pAttachments = attachments,
+				.attachmentCount = attachments.size(),
+				.pAttachments = attachments.data(),
 				.width = swapChainExtent.width,
 				.height = swapChainExtent.height,
 				.layers = 1,
@@ -1145,6 +1218,59 @@ private:
 		vkFreeMemory(device, stagingBufferMemory, nullptr);
 	}
 
+	void createDepthResources(void)
+	{
+		VkFormat depthFormat = findDepthFormat();
+
+		createImage(swapChainExtent.width, swapChainExtent.height, depthFormat,
+			    VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+			    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, &depthImage, &depthImageMemory,
+			    "depth buffer");
+
+		depthImageView = createImageView(depthImage, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT);
+
+		transitionImageLayout(depthImage, depthFormat,
+				      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
+	}
+
+	VkFormat findSupportedFormat(const std::vector<VkFormat> &candidates,
+				     VkImageTiling tiling, VkFormatFeatureFlags features)
+	{
+		for (VkFormat format : candidates) {
+			VkFormatProperties props;
+
+			vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
+
+			if (tiling == VK_IMAGE_TILING_LINEAR) {
+				if ((props.linearTilingFeatures & features) == features) {
+					return format;
+				}
+			} else if (tiling == VK_IMAGE_TILING_OPTIMAL){
+				if ((props.optimalTilingFeatures & features) == features) {
+					return format;
+				}
+			} else {
+				PANIC_VK("invalid tiling");
+			}
+		}
+
+		PANIC_VK("failed to find supported format");
+	}
+
+	VkFormat findDepthFormat(void)
+	{
+		return findSupportedFormat(
+			{ VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+			VK_IMAGE_TILING_OPTIMAL,
+			VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
+		);
+	}
+
+	bool hasStencilComponent(VkFormat format)
+	{
+		return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
+	}
+
 	void createCommandPool(void)
 	{
 		QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
@@ -1172,7 +1298,9 @@ private:
 		CALL_VK(vkBeginCommandBuffer(commandBuffer, &beginInfo),
 			"failed to begin recording command buffer");
 
-		VkClearValue clearColor = {{{ 0.1f, 0.1f, 0.1f, 1 }}};
+		std::array<VkClearValue, 2> clearValues;
+		clearValues.at(0).color = {{ 0.1f, 0.1f, 0.1f, 1 }};
+		clearValues.at(1).depthStencil = { 1, 0 };
 
 		VkRenderPassBeginInfo renderPassInfo = {
 			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
@@ -1183,8 +1311,8 @@ private:
 				.offset = { 0, 0 },
 				.extent = swapChainExtent,
 			},
-			.clearValueCount = 1,
-			.pClearValues = &clearColor,
+			.clearValueCount = clearValues.size(),
+			.pClearValues = clearValues.data(),
 		};
 
 		VkViewport viewport = {
@@ -1492,6 +1620,19 @@ private:
 
 			srcStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
 			dstStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		} else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED &&
+			   newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+			barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+			if (hasStencilComponent(format)) {
+				barrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+			}
+
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT |
+						VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+			srcStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+			dstStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
 		} else {
 			PANIC_VK("unsupported layout transition");
 		}
@@ -1777,13 +1918,13 @@ private:
 				// break;
 				return VK_FALSE;
 			case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
-				std::cerr << "[VALIDATION INFO]: ";
+				std::cerr << Color::blue << "[VALIDATION INFO]: " << Color::reset;
 				break;
 			case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
-				std::cerr << "[VALIDATION WARNING]: ";
+				std::cerr << Color::yellow << "[VALIDATION WARNING]: " << Color::reset;
 				break;
 			case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
-				std::cerr << "[VALIDATION ERROR]: ";
+				std::cerr << Color::red << "[VALIDATION ERROR]: " << Color::reset;
 				break;
 			default:
 				PANIC_VK("unknown message severity");
