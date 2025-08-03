bb670759c81400547253d75f32bc9de4bc4117c3
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2025-08-03 22:11:34 +0300

    introduce window class
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

---
+++ kwindow.hpp
@@ -0,0 +1,75 @@
+
+#ifndef KWINDOW_HPP
+#define KWINDOW_HPP
+
+#include <GLFW/glfw3.h>
+
+#include <utility>
+
+struct kwindow_info {
+	unsigned width    = 800;
+	unsigned height   = 600;
+	const char *title = "";
+	bool resizable    = false;
+};
+
+struct kwindow {
+	kwindow(void) {}
+	~kwindow(void) {}
+
+	void create(struct kwindow_info *create_info)
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, create_info->resizable);
+
+		this->window = glfwCreateWindow(create_info->width,
+						create_info->height,
+						create_info->title,
+						nullptr, nullptr);
+
+		// glfwSetWindowUserPointer(window, this);
+		// glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+	}
+
+	void destroy(void)
+	{
+		glfwDestroyWindow(this->window);
+		glfwTerminate();
+	}
+
+	bool should_close(void)
+	{
+		return glfwWindowShouldClose(this->window);
+	}
+
+	std::pair<unsigned, unsigned> get_framebuffer_size(void) const
+	{
+		int w, h;
+
+		glfwGetFramebufferSize(this->window, &w, &h);
+		return {w, h};
+	}
+
+	void wait_events(void) const
+	{
+		glfwWaitEvents();
+	}
+
+	void poll_events(void) const
+	{
+		glfwPollEvents();
+	}
+
+	GLFWwindow *__get_window(void) const
+	{
+		return this->window;
+	}
+
+private:
+	GLFWwindow *window;
+
+};
+
+#endif /* KWINDOW_HPP */
