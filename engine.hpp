bb68a45e7ec679e8093e0cf13235f9a4ad77c860
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2025-09-27 21:40:27 +0300

    v4: add surface
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

--- engine.hpp
+++ engine.hpp
@@ -24,28 +24,33 @@ private:
         // init_vulkan functions
         void create_instance(void);
         void setup_debug_messanger(void);
+        void create_surface();
         void pick_physical_device(void);
         void create_logical_device(void);
     void main_loop(void);
     void cleanup(void);
 
     // util functions
-    static uint32_t                  get_graphics_family_index(const vk::raii::PhysicalDevice &pd);
-    static int                       get_physical_device_score(const vk::raii::PhysicalDevice &pd);
-    static std::vector<const char *> get_required_device_extensions();
-    static std::vector<const char *> get_required_instance_extensions();
-    static vk::Bool32                debug_callback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
-                                                    vk::DebugUtilsMessageTypeFlagsEXT type,
-                                                    const vk::DebugUtilsMessengerCallbackDataEXT *callback_data, void *);
+    static std::pair<uint32_t, uint32_t> get_graphics_present_family_indexes(const vk::raii::PhysicalDevice &pd,
+                                                                             const vk::raii::SurfaceKHR &surface);
+    static int                           get_physical_device_score(const vk::raii::PhysicalDevice &pd);
+    static std::vector<const char *>     get_required_device_extensions();
+    static std::vector<const char *>     get_required_instance_extensions();
+    static vk::Bool32                    debug_callback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+                                                        vk::DebugUtilsMessageTypeFlagsEXT type,
+                                                        const vk::DebugUtilsMessengerCallbackDataEXT *callback_data,
+                                                        void *);
 
 private:
     GLFWwindow                       *window;
     vk::raii::Context                context;
-    vk::raii::Instance               instance = nullptr;
+    vk::raii::Instance               instance        = nullptr;
     vk::raii::DebugUtilsMessengerEXT debug_messenger = nullptr;
+    vk::raii::SurfaceKHR             surface         = nullptr;
     vk::raii::PhysicalDevice         physical_device = nullptr;
-    vk::raii::Device                 device = nullptr;
-    vk::raii::Queue                  graphics_queue = nullptr;
+    vk::raii::Device                 device          = nullptr;
+    vk::raii::Queue                  graphics_queue  = nullptr;
+    vk::raii::Queue                  present_queue   = nullptr;
 
 };
 
