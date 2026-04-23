8f44b23227c9eca4b7b49d66b687771c40850078
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2026-04-23 23:55:41 +0300

    v5: added vulkan
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

--- v5/main.py
+++ v5/main.py
@@ -1,16 +1,20 @@
 
-import vulkan
+import vulkan as vk
 import glfw
 
 from typing import final
 
+GLFWwindow = glfw._GLFWwindow
+
 @final
 class HelloTriangle:
-    def __init__(self, width, height):
-        self.window = None
+    def __init__(self, width: int, height: int):
+        self.window: GLFWwindow
         self.width = width
         self.height = height
+
         self._init_window()
+        self._create_instance()
 
     def run(self):
         while not glfw.window_should_close(self.window):
@@ -28,6 +32,20 @@ class HelloTriangle:
         glfw.window_hint(glfw.RESIZABLE, glfw.FALSE)
 
         self.window = glfw.create_window(self.width, self.height, "vulkan", None, None)
+
+    def _create_instance(self):
+        appInfo = vk.VkApplicationInfo(
+            pApplicationName="hello triangle",
+            applicationVersion=vk.VK_MAKE_VERSION(1, 0, 0),
+            pEngineName="no engine",
+            engineVersion=vk.VK_MAKE_VERSION(1, 0, 0),
+            apiVersion=vk.VK_MAKE_VERSION(1, 4, 0),
+        )
+
+        createInfo = vk.VkInstanceCreateInfo(
+            pApplicationInfo = appInfo,
+        )
+
 
 
 
